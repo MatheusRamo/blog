b2c50cbe095a6fc6766ceb537418c9de3019535e
Matheus Ramos <geof.matheus@gmail.com>
2024-07-01 08:36:42 -0300

    addd

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Matheus Ramos',
+  author: 'Matheus Ramos',
+  headerTitle: 'Matheus Ramos',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
@@ -11,15 +11,12 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  x: 'https://twitter.com/x',
-  // twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
+  email: 'geof.matheus@gmail.com',
+  github: 'https://github.com/MatheusRamo',
+  x: 'https://twitter.com/GeofMatheus',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
+  linkedin: 'https://www.linkedin.com/in/matheus-ramos-44730b181/',
+  instagram: 'https://www.instagram.com/math.ramo/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
